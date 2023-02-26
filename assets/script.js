76e60bed08bfbbfccd289a22daf57fef94fb610b
Nephew20 <cofieldk20@gmail.com>
2023-02-26 15:41:34 -0500

    added link to the dayjs and displayed current day on html

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// Displays the current day on the html
+var dayEl = $('#currentDay')
+var currentDay = dayjs().format('ddd, MMM DD, YYYY');
+dayEl.text(currentDay)
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in

f5bcc594f183c6193c1c17789a79fa0c5c999757
Nephew20 <cofieldk20@gmail.com>
2024-08-20 16:05:13 -0400

    finish script.js

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,28 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
 
   var hoursEl = $('#hours');
-  
-  for(i=0; i<9; i++) {
+
+  for (i = 0; i < 9; i++) {
     var divEl = $('<div>')
     var textArea = $('<textarea>')
     var button = $('<button>')
     var iEl = $('<i>')
-    var hour = dayjs().hour(i+9).minute(0)
+    var hour = dayjs().hour(i + 9).minute(0)
     var currentHour = dayjs()
 
     console.log('this works')
 
     //Text box for the days
     textArea.addClass('col-8 col-md-10 description').attr('rows', '3')
-    
+
     //Save Button
     button.addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save')
 
     iEl.addClass('fas fa-save').attr('aria-hidden', 'true')
-    
+
     //Container for the text box, button and hour label
     divEl.addClass('row  time-block hour text-center ps-10').attr('id', hour.format('h A'))
-    
+
 
     //Hour label
     var hourLabel = $('<div>').addClass('col-2 col-md-1 hour text-center').text(hour.format('h A'))
@@ -34,41 +30,38 @@ $(function () {
     button.append(iEl)
 
     //Time Comparison using dayJs
-    if (hour.isBefore(currentHour, 'h')){
-       divEl.addClass('past')
-     } else if (hour.isSame(currentHour, 'h')) {
-       divEl.addClass('present')
-     } else {
-       divEl.addClass('future')
-     }
-    
+    if (hour.isBefore(currentHour, 'h')) {
+      divEl.addClass('past')
+    } else if (hour.isSame(currentHour, 'h')) {
+      divEl.addClass('present')
+    } else {
+      divEl.addClass('future')
+    }
+
     // Adding compenents to the container
     divEl.append(hourLabel, textArea, button)
     hoursEl.append(divEl)
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
-  button.on('click', function () {
-    var todo = $(this).closest('.hour').find('textArea').val()
-    console.log(todo)
-    var todoID = $(this).closest('.hour').attr('id')
-    console.log(todoID)
-    localStorage.setItem(todoID, todo)
-  })
-}
-  
-  
-  
+    //Event listener to save the info in the text boxes to localstorage
+    button.on('click', function () {
+      var todo = $(this).closest('.time-block').find('textArea').val()
+      console.log(todo)
+      var todoID = $(this).closest('.time-block').attr('id')
+      console.log(todoID)
+      localStorage.setItem(todoID, todo)
+    })
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  
+    //Renders and keeps the info in the text boxes after refreshing the page
+    divEl.each(function () {
+      var todoID = $(this).attr('id')
+      todos = localStorage.getItem(todoID)
+      $(this).find('textarea').val(todos)
+    })
+
+
+
+  }
+
   // Displays the current day on the html
   var dayEl = $('#currentDay')
   var currentDay = dayjs().format('ddd, MMM DD, YYYY');
